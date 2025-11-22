03602b4a349ecac1b9037b29be35448ad7f17c9e
Abdelaal3 <mahmoudabdelaal672@gmail.com>
2025-11-22 20:00:51 +0200

    Adding Script Api

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ try {
         {
             headers: {
                 "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-                "Accept": "application/json",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "ar,en-US;q=0.9,en;q=0.8",
                 "Referer": "https://jdwel.com/today/",
-                "Origin": "https://jdwel.com"
+                "Origin": "https://jdwel.com",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Dest": "empty",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive",
             },
         }
     );
