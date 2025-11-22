d73e463142138e958e8dfee5217c3d9cad74bde0
Mahmoud Abdelaal <mahmoudabdelaal672@gmail.com>
2025-11-22 17:36:39 +0200

    Adding

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 import express from "express";
 import axios from "axios";
-import * as cheerio from "cheerio";
 
 const app = express();
 
-app.get("/news", async (req, res) => {
-  const url = "https://www.alwafd.news/5596662";
-
+app.get("/today", async (req, res) => {
   try {
-    const { data } = await axios.get(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-      },
-    });
+    const { data } = await axios.get(
+      "https://jdwel.com/wp-json/jmanager/web/v1/live/matches/"
+    );
 
-    const $ = cheerio.load(data);
-
-    const title = $(".news-post h1").text().trim();
-    const date = $(".info time").text().trim();
-
-    const paragraphs = [];
-    $(".paragraph-list p").each((i, el) => {
-      paragraphs.push($(el).text().trim());
-    });
-
-    res.json({
-      title,
-      date,
-      paragraphs,
-    });
-
+    res.json(data);
   } catch (err) {
-    res.status(500).json({
-      error: "Scraping failed",
-      details: err.message,
-    });
+    res.status(500).json({ error: "Request failed", details: err.message });
   }
 });
 
